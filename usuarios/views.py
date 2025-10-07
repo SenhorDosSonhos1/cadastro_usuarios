c0d73912a62a4ac066c640c27d51a1e83770a64f
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2025-10-07 09:04:39 -0300

    Adicionando mensagens de error

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 # Create your views here.
 
 def usuario_cadastro(request):
@@ -10,9 +12,14 @@ def usuario_cadastro(request):
         nome_usuario = request.POST.get("username")
         email = request.POST.get("email")
         senha = request.POST.get("password")
+        #Adicionar validação
         confirmar_senha = request.POST.get("confirm_password")
         
-        if User.objects.filter(email = email).exists():
+        if senha != confirmar_senha:
+            messages.error(request, "As senhas não coincidem")
+            return redirect(reverse('cadastro'))
+        
+        if User.objects.filter(username = nome_usuario, email = email).exists():
             return HttpResponse("Usuario ja existe")
         
         user = User.objects.create_user(
@@ -26,15 +33,14 @@ def usuario_cadastro(request):
 
 def usuario_login(request):
     if request.method == "POST":
-        email = request.POST.get("email")
+        nome_usuario = request.POST.get("username")
         senha = request.POST.get("password")
 
-        usuario = authenticate(email = email, password = senha)
+        usuario = authenticate(username = nome_usuario, password = senha)
 
         if usuario is not None:
             login(request, usuario)
             return HttpResponse("Logado")
-            login(request, usuario)
         return HttpResponse("Não existe um usuario com essas credenciais")
             
     return render(request, 'login.html')
